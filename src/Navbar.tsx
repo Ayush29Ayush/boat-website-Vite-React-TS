ece4d5b42598b38ef42e34e6c9a1664e0b047cbb
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-05 20:48:16 +0530

    refactor: navbar UI changes

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -6,7 +6,16 @@ function Navbar() {
     <div className="w-full flex items-center justify-around h-80px fixed top-0 bg-white z-100">
       {/* Navbar */}
       <Logo />
-      <ArrowRight />
+      <div className="flex gap-20px items-center <md:hidden>">
+        <span className="cursor-pointer">solutions.</span>
+        <span className="cursor-pointer">courses.</span>
+        <span className="cursor-pointer">resources.</span>
+        <span className="cursor-pointer">about.</span>
+      </div>
+      <div className="flex gap-5px items-center cursor-pointer">
+        <span className="mb-3px">sign in</span>
+        <ArrowRight />
+      </div>
     </div>
   );
 }
