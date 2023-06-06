7c46993100dafca8ef9dbd65bd6e84d9d3320b71
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-06 11:59:18 +0530

    style: animation added to MapImages Component

--- src/MapImages.tsx
+++ src/MapImages.tsx
@@ -1,8 +1,31 @@
+import { motion } from "framer-motion";
+import { animationStart } from "./utils/animation";
+
 function MapImages() {
   return (
     <div className="absolute w-full flex justify-between transform -translate-y-200px -z-1 overflow-hidden">
-      <img src="src/assets/map_1.png" />
-      <img src="src/assets/map_2.png" />
+      <motion.img
+        src="src/assets/map_1.png"
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: animationStart + 1.2,
+          type: "tween",
+          duration: 0.5,
+        }}
+        className="object-cover"
+      />
+      <motion.img
+        src="src/assets/map_2.png"
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: animationStart + 1.2,
+          type: "tween",
+          duration: 0.5,
+        }}
+        className="object-cover"
+      />
     </div>
   );
 }
